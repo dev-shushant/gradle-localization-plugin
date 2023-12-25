96a0beeb249310f4eb69f5f08ce609d4a104e654
shushanttiwari <shushanttiwari.ashu@gmail.com>
2023-12-25 19:22:35 +0530

    Intiial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "dev.shushant"
-version = "1.0.0"
+version = "1.0.0.0"
 
 repositories {
     mavenCentral()
@@ -17,29 +17,21 @@ repositories {
 }
 
 gradlePlugin {
-    website = "https://github.com/dev-shushant/gradle-localization-plugin.git"
+    website = "https://www.shushant.dev/"
     vcsUrl = "https://github.com/dev-shushant/gradle-localization-plugin.git"
     plugins {
         create("localizationPlugin") {
             id = "dev.shushant.localization.plugin"
             implementationClass = "dev.shushant.localization.plugin.LocalizationPlugin"
             displayName = "App localization plugin"
-            description = """
-        Simplify and streamline the localization process for your Android app with the App Localization Plugin.
-        This Gradle plugin automates key aspects of localization, offering a seamless integration into your build process. Say goodbye to tedious manual translation management and ensure a consistent user experience across different languages.
-        Whether you're a solo developer or part of a team, the App Localization Plugin empowers you to internationalize your app effortlessly. Focus on building great features while leaving the localization complexities to this versatile Gradle plugin.
-        Get started with just a few lines of configuration and unlock the full potential of globalizing your Android app!
-    """
+            description = "Simplify and streamline the localization process for your Android app with the App Localization Plugin.\n"
             tags = listOf("localization", "androidApp", "composeSupport")
         }
         create("generateAppResourceClass") {
             id = "dev.shushant.generateAppResourceClass.plugin"
             implementationClass = "dev.shushant.localization.plugin.GenerateAppResourceClassPlugin"
             displayName = "Generate app resource classes"
-            description = """
-Simplify resource management in your Jetpack Compose project with the Generate App Resource Class plugin. This Gradle plugin automates the creation of a Kotlin class that acts as a central hub for accessing application resources such as strings and drawables directly from your Composables.
-Enhance your Compose development workflow by leveraging the Generate App Resource Class plugin. Spend less time managing resources and more time building delightful user interfaces!
-            """.trimIndent()
+            description = "Simplify resource management in your Jetpack Compose project with the Generate App Resource Class plugin. This Gradle plugin automates the creation of a Kotlin class that acts as a central hub for accessing application resources such as strings and drawables directly from your Composables."
             tags = listOf(
                 "android",
                 "jetpack",
